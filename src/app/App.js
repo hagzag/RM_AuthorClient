ee4bb5bdbe6260121e3888959496a9625fb83534
hawarez <erez@alcide.io>
2017-07-10 12:40:35 +0300

    road map table

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import RoadmapTable from "./components/RoadmapTable";
 
 export default class App extends Component {
   render() {
     return (
-      <div>authors</div>
+      <div>
+        <div>authors</div>
+        <RoadmapTable />
+      </div>
     );
   }
 }
---
+++ src/app/components/RoadmapTable.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { Table } from 'react-bootstrap';
+
+export default class RoadmapTable extends Component {
+  renderRoadmap = rm => (
+    <tr>
+      <td>{rm.title}</td>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+  );
+
+  render() {
+    return (
+      <Table striped bordered condensed hover>
+        <thead>
+        <tr>
+          <th>Title</th>
+          <th>Version</th>
+          <th># Tutors</th>
+          <th># Students</th>
+          <th># Graduates</th>
+          <th>Ratings</th>
+        </tr>
+        </thead>
+        <tbody>
+          {this.props.roadmaps.map(roadmap => this.renderRoadmap(roadmap))}
+        </tbody>
+      </Table>
+    );
+  }
+}
