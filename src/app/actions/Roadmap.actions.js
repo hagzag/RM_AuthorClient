404c3ed6ab1ecdf56f9ad6b0abbca1b0a6bebb55
hawarez <erez@alcide.io>
2017-07-10 16:44:40 +0300

    axios calls

--- src/app/actions/Roadmap.actions.js
+++ src/app/actions/Roadmap.actions.js
@@ -1,11 +1,36 @@
 import { STORE_ROADMAPS } from './index.actions';
 import { RoadmapsMock } from '../../mock/Roadmap.mock';
+import axios from 'axios';
 
 export const getRoadmaps = authorId => dispatch => {
-  // return axios.post('roadmaps', { authorId: authorId })
-  //   .then(res => {
-  //     dispatch({ type: STORE_ROADMAPS, roadmaps: res.data.data });
-  //   })
-  //   .catch(err => console(err));
-  dispatch({ type: STORE_ROADMAPS, roadmaps: RoadmapsMock });
+  return axios.get('roadmaps', { name})
+    .then(res => {
+      dispatch({ type: STORE_ROADMAPS, roadmaps: res.data.data });
+    })
+    .catch(err => console.log(err));
+  // dispatch({ type: STORE_ROADMAPS, roadmaps: RoadmapsMock });
+};
+
+export const addRoadmap = roadmap => dispatch => {
+  return axios.post('roadmaps', { roadmapId: '111' })
+    .then(res => {
+      dispatch(getRoadmaps());
+    })
+    .catch(err => console.log(err));
+};
+
+export const deleteRoadmap = roadmapId => dispatch => {
+ return axios.delete('roadmaps', { roadmapid: roadmapId })
+   .then(res => {
+     dispatch(getRoadmaps());
+   })
+   .catch(err => console.log(err));
+};
+
+export const eritRoadmap = roadmap => dispatch => {
+  return axios.put('roadmaps', { roadmap: roadmap })
+    .then(res => {
+      dispatch(getRoadmaps());
+    })
+    .catch(err => console.log(err));
 };
