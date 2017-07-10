bb712b4ba55ade8f96b4a970660e2fe6270eb7e5
herzaso <herzaso@gmail.com>
2017-07-10 15:44:18 +0300

    roadmap table

--- src/app/App.js
+++ src/app/App.js
@@ -2,11 +2,18 @@ import React, { Component } from 'react';
 import RoadmapTable from "./components/RoadmapTable";
 
 export default class App extends Component {
+  data = [
+      {id:1, title:'foo', version:1.0, num_tutors:5, num_students:2, num_grads:10, rating:5},
+      {id:2, title:'bar', version:1.0, num_tutors:5, num_students:2, num_grads:10, rating:2},
+      {id:3, title:'baz', version:1.0, num_tutors:5, num_students:2, num_grads:10, rating:4},
+      {id:4, title:'foo', version:1.0, num_tutors:5, num_students:2, num_grads:10, rating:1},
+  ]
+
   render() {
     return (
       <div>
         <div>authors</div>
-        <t-stars value="5"></t-stars>
+        <RoadmapTable data={this.data} />
       </div>
     );
   }
