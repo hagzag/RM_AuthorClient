0dd966500a5f47dd9ea56fe368fdb6f83a6198b4
Ortal Yedgar <ortaly@Ortals-MacBook-Pro-2.local>
2017-07-10 13:55:25 +0300

    add webco

--- src/app/App.js
+++ src/app/App.js
@@ -6,7 +6,7 @@ export default class App extends Component {
     return (
       <div>
         <div>authors</div>
-        <RoadmapTable />
+        <t-stars value="5"></t-stars>
       </div>
     );
   }
